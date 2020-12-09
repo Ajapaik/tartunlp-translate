135c464b89202aabe2dcdb5f0855397a4cda1373
Lauri Elias <laurileet@gmail.com>
2020-12-09 02:07:08 +0200

    Update example

--- test_socket.py
+++ test_socket.py
@@ -2,11 +2,21 @@ import json
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-client_socket.connect(('127.0.0.1', 12346))
+client_socket.connect(('95.216.222.78', 12346))
 client_socket.send(json.dumps({
-    'src': 'Tere',
-    'conf': ','
+    'src': 'Kaader filmist "Test3" 0:02:59.509',
+    'conf': 'en,'
 }).encode())
 data = client_socket.recv(1024)
-print(data.decode())
+print(json.loads(data.decode())['final_trans'])
+client_socket.close()
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('95.216.222.78', 12346))
+client_socket.send(json.dumps({
+    'src': 'Kaader filmist "Test3" 0:02:59.509',
+    'conf': 'lv,'
+}).encode())
+data = client_socket.recv(1024)
+print(json.loads(data.decode())['final_trans'])
+client_socket.close()
